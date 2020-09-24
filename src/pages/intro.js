20a224f0d7600a02e24228bd33b49ac9abc8897f
Alexander Chen <alexanderchen@Alexanders-MacBook-Pro.local>
2020-09-24 10:18:39 -0400

    added page templates

---
+++ src/pages/intro.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Link } from "react-scroll";
+
+import "./styles.css";
+
+class Intro extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Intro
---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "react-scroll";
+
+import "./styles.css";
+
+class Projects extends Component {
+  render() {
+    return (
+    )
+  }
+}
+
+export default Projects
