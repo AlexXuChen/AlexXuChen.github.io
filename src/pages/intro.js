5cdad2391b68acf876bf0c67e2a4d151f259b744
Alexander Chen <alexanderchen@Alexanders-MacBook-Pro.local>
2020-09-25 17:05:13 -0400

    initialized typist block for introduction

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { Link } from "react-scroll";
+import Typist from 'react-typist'
+// import { Link } from "react-scroll";
 
 import "./styles.css";
 
 class Intro extends Component {
   render() {
     return (
-
+        <div id="intro">
+            <Typist>
+                <p>Hello!</p>
+                <p>My name is Alexander Chen</p>
+                <p>Computer Science Student at the University of Toronto St.George Campus</p>
+                <p>Welcome to my website!</p>
+            </Typist>
+        </div>
     )
   }
 }
