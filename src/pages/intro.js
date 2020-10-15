a0d628575e1c2a44c1f3601e5f27cdbaa8af3277
AlexXuChen <alex.xu.chen@gmail.com>
2020-10-15 12:27:35 -0400

    added profile pic to intro

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -9,21 +9,26 @@ class Intro extends Component {
   render() {
     return (
         <div className="intro">
-            <Typist avgTypingDelay={30} cursor={{ hideWhenDone: true }}>
-                <p>Hi there, my name is {' '}
-                    <span className="highlight">Alexander Chen</span>
-                    .</p>
-                <Typist.Delay ms={500} />
-                <p>I'm a {' '}
-                    <span className="highlight">Software Developer</span> 
-                    {' '} studying {' '}
-                    <span className="highlight">Computer Science</span>
-                    {' '} at the {' '}
-                    <span className="highlight">University of Toronto</span>
-                    .</p>
-                <Typist.Delay ms={500} />
-                <p>Welcome to my website!</p>
-            </Typist>
+            <div className="introText">
+                <Typist avgTypingDelay={40} cursor={{ hideWhenDone: true }}>
+                    <p>Hi there, my name is {' '}
+                        <span className="highlight">Alexander Xu Chen</span>
+                        .</p>
+                    <Typist.Delay ms={1000} />
+                    <p>I'm a {' '}
+                        <span className="highlight">Software Developer</span> 
+                        {' '} studying {' '}
+                        <span className="highlight">Computer Science</span>
+                        {' '} at the {' '}
+                        <span className="highlight">University of Toronto</span>
+                        .</p>
+                    <Typist.Delay ms={500} />
+                    <p>Welcome to my website!</p>
+                </Typist>
+            </div>
+            <div className="profileContainer">
+                <img className="profile" src={require('../res/profile.jpg')} alt="profile"/>
+            </div>
         </div>
     )
   }
