b6ebe538ae04c6ddecbc21587a75caf6cc685ad9
AlexXuChen <alex.xu.chen@gmail.com>
2020-10-16 03:50:41 -0400

    added navbar component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Links from "./pages/links"
 import Intro from "./pages/intro"
 import Projects from "./pages/projects"
+import Navbar from "./components/navbar"
+import Work from "./pages/work"
 
 function App() {
   return (
     <div className="App">
-      <Intro></Intro>
-      <Links></Links>
+      <Navbar></Navbar>
+        <Intro></Intro>
+        <Links></Links>
+        <Work></Work>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import "./styles.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+          <a className="navbar-brand navName" href="#">
+            <img
+              src={require("../res/profile.jpg")}
+              className="profile d-inline-block align-top"
+              alt=""
+            />
+          </a>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    Alexander Xu Chen
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Work
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Projects
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Stack
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
