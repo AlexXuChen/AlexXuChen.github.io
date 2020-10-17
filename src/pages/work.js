3e55b3d15e1b596c68302279967f028248a6749f
AlexXuChen <alex.xu.chen@gmail.com>
2020-10-17 15:09:59 -0400

    added work section template

---
+++ src/pages/work.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./styles.css";
+
+class Work extends Component {
+  render() {
+    return (
+      <div className="work">
+        
+      </div>
+    )
+  }
+}
+
+export default Work
