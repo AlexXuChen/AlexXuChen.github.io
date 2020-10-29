8a1449b5bdd56aa54d560fb9d2d16e7a4ff37839
AlexXuChen <alex.xu.chen@gmail.com>
2020-10-29 12:36:40 -0400

    added navbar and links component to intro page

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Typist from 'react-typist'
 // import { Link } from "react-scroll";
+import Navbar from "../components/navbar"
+import Links from "../components/links"
 
 import "./styles.css";
 
@@ -8,24 +10,28 @@ import "./styles.css";
 class Intro extends Component {
   render() {
     return (
-        <div className="intro">
-            <div className="introText">
-                <Typist avgTypingDelay={40} cursor={{ hideWhenDone: true }}>
-                    <p>Hi there, my name is {' '}
-                        <span className="highlight">Alexander Xu Chen</span>
-                        .</p>
-                    <Typist.Delay ms={1000} />
-                    <p>
-                        <span className="highlight">Software Developer</span> 
-                        {' '}studying{' '}
-                        <span className="highlight">Computer Science</span>
-                        {' '} @ {' '}
-                        <span className="highlight">University of Toronto</span>
-                        .</p>
-                    <Typist.Delay ms={500} />
-                    <p>Welcome to my website!</p>
-                </Typist>
+        <div>
+            <Navbar />
+            <div className="intro">
+                <div className="introText">
+                    <Typist avgTypingDelay={40} cursor={{ hideWhenDone: true }}>
+                        <p>Hi there, my name is {' '}
+                            <span className="highlight">Alexander Xu Chen</span>
+                            .</p>
+                        <Typist.Delay ms={1000} />
+                        <p>
+                            <span className="highlight">Software Developer</span> 
+                            {' '}studying{' '}
+                            <span className="highlight">Computer Science</span>
+                            {' '} @ {' '}
+                            <span className="highlight">University of Toronto</span>
+                            .</p>
+                        <Typist.Delay ms={500} />
+                        <p>Welcome to my website!</p>
+                    </Typist>
+                </div>
             </div>
+            <Links />
         </div>
     )
   }
