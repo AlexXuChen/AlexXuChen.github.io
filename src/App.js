45346476ef01eeec43a27aebe303da23222d36dc
AlexXuChen <alex.xu.chen@gmail.com>
2020-10-29 12:35:36 -0400

    added react router to main app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import './App.css';
-import Links from "./pages/links"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Intro from "./pages/intro"
 import Projects from "./pages/projects"
-import Navbar from "./components/navbar"
 import Work from "./pages/work"
+import Stack from "./pages/stack"
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-        <Intro></Intro>
-        <Links></Links>
-        <Work></Work>
-    </div>
+    <Router>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Intro />
+          </Route>
+          <Route path="/work">
+            <Work />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/stack">
+            <Stack />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
