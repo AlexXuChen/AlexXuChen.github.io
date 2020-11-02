282b0a17434e10a61922fd714449c66136d22c8b
AlexXuChen <alex.xu.chen@gmail.com>
2020-11-01 21:00:36 -0500

    added carosel to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 
-
 import "./styles.css";
 
 class Projects extends Component {
   render() {
     return (
-      <div className="projects"> 
-        
+      <div className="projects">
+        <div class="projectCards container-fluid">
+          <div class="row flex-row flex-nowrap">
+            
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Projects
+export default Projects;
