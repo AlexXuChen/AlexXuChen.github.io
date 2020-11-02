55df96ca28a488b513dce16409b501a28f169940
AlexXuChen <alex.xu.chen@gmail.com>
2020-11-01 19:40:58 -0500

    added vertical cards to work page

--- src/pages/work.js
+++ src/pages/work.js
@@ -6,10 +6,28 @@ class Work extends Component {
   render() {
     return (
       <div className="work">
-        Hello
+        <div class="container-fluid">
+          <div class="row flex-row flex-nowrap">
+            <div class="col-3">
+              <div class="card card-block">Card</div>
+            </div>
+            <div class="col-3">
+              <div class="card card-block">Card</div>
+            </div>
+            <div class="col-3">
+              <div class="card card-block">Card</div>
+            </div>
+            <div class="col-3">
+              <div class="card card-block">Card</div>
+            </div>
+            <div class="col-3">
+              <div class="card card-block">Card</div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Work
+export default Work;
