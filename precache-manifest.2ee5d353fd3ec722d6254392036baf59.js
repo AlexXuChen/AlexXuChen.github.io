04eaab4dddcbd6bac8d37aa5da72def7e02c6862
AlexXuChen <alex.xu.chen@gmail.com>
2021-04-21 14:42:03 -0400

    Updates

---
+++ precache-manifest.2ee5d353fd3ec722d6254392036baf59.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6e94d65b977103e34399b9df836010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a521db621bf9380de547",
+    "url": "/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "0e1b2d05fd4e14738935",
+    "url": "/static/css/main.b1ed5d20.chunk.css"
+  },
+  {
+    "revision": "a521db621bf9380de547",
+    "url": "/static/js/2.c8274f54.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c8274f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1b2d05fd4e14738935",
+    "url": "/static/js/main.47dde545.chunk.js"
+  },
+  {
+    "revision": "2470e9b25e276ba96a23",
+    "url": "/static/js/runtime-main.ab7119da.js"
+  },
+  {
+    "revision": "a3978104f5cfd0144c0c6145412c5bff",
+    "url": "/static/media/achievementhub.a3978104.png"
+  },
+  {
+    "revision": "62e48afd619854b441a82d0945c368e5",
+    "url": "/static/media/apothecary.62e48afd.bmp"
+  },
+  {
+    "revision": "e7f981d0ac54517765c25018e8039ba0",
+    "url": "/static/media/arcticminers.e7f981d0.jpg"
+  },
+  {
+    "revision": "32638b63e02e6bbc25d8d8969ca49946",
+    "url": "/static/media/bell.32638b63.bmp"
+  },
+  {
+    "revision": "3d9e7188c626bb5d5ae855115365ce2c",
+    "url": "/static/media/bellsmartcity.3d9e7188.png"
+  },
+  {
+    "revision": "0a71e1f06bda52bc80b41f7805362e54",
+    "url": "/static/media/cssu.0a71e1f0.bmp"
+  },
+  {
+    "revision": "1927f1a00c866a6a4e5f951dc02456fd",
+    "url": "/static/media/desperado.1927f1a0.png"
+  },
+  {
+    "revision": "eb9d9471fe80ba11727effb936a46f14",
+    "url": "/static/media/doctor.eb9d9471.jpeg"
+  },
+  {
+    "revision": "db7af838a5cca40603600eee66c332a8",
+    "url": "/static/media/pixelpenciljs.db7af838.jpg"
+  },
+  {
+    "revision": "e68dab92ab2feb86603bd6f55f25569f",
+    "url": "/static/media/profile.e68dab92.jpg"
+  },
+  {
+    "revision": "3b98d3e03cf93d5384ce4d0571dac1ed",
+    "url": "/static/media/redhat.3b98d3e0.bmp"
+  },
+  {
+    "revision": "e0deeb713d3a3ed057be841bf99cbae7",
+    "url": "/static/media/robodropper.e0deeb71.jpg"
+  },
+  {
+    "revision": "0d20a47de9000eab872a2e4c90024da5",
+    "url": "/static/media/safebet.0d20a47d.jpg"
+  },
+  {
+    "revision": "209603c255d40086da2fe89ffe69819e",
+    "url": "/static/media/starbucks.209603c2.bmp"
+  },
+  {
+    "revision": "a87e191286544517ca99889ad4c6ef34",
+    "url": "/static/media/twiliochatbot.a87e1912.png"
+  }
+]);
