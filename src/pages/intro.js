1a503f6211723e72d3ffeb76de82c3004c66f210
AlexXuChen <alex.xu.chen@gmail.com>
2021-04-22 17:52:59 -0400

    fixed new line typist

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,37 +1,37 @@
 import React, { Component } from "react";
-import Typist from 'react-typist'
-import Links from "../components/links"
+import Typist from "react-typist";
+import Links from "../components/links";
 
 import "./styles.css";
 
 class Intro extends Component {
   render() {
     return (
-        <div>
-            <div className="intro">
-                <div className="introText">
-                    <Typist avgTypingDelay={20} cursor={{ hideWhenDone: true }}>
-                        <p>Hi there, my name is {' '}
-                            <span className="highlight">Alexander Xu Chen</span>
-                            .</p>
-                        <Typist.Delay ms={1000} />
-                        <p>
-                            I'm a{' '}
-                            <span className="highlight">Software Developer</span> 
-                            {' '}studying{' '}
-                            <span className="highlight">Computer Science</span>
-                            {' '} @ {' '}
-                            <span className="highlight">University of Toronto</span>
-                            .</p>
-                        <Typist.Delay ms={500} />
-                        <p>Welcome to my website!</p>
-                    </Typist>
-                </div>
-            </div>
-            <Links />
+      <div>
+        <div className="intro">
+          <div className="introText">
+            <Typist avgTypingDelay={10} cursor={{ hideWhenDone: true }}>
+              <p className="intro-p">
+                Hi there, my name is{" "}
+                <span className="highlight">Alexander Xu Chen</span>.
+              </p>
+              <Typist.Delay ms={1000} />
+              <br />
+              <p className="intro-p">
+                I'm a <span className="highlight">Software Developer</span>{" "}
+                studying <span className="highlight">Computer Science</span> @{" "}
+                <span className="highlight">University of Toronto</span>.
+              </p>
+              <Typist.Delay ms={1000} />
+              <br />
+              <p className="intro-p">Welcome to my website!</p>
+            </Typist>
+          </div>
         </div>
-    )
+        <Links />
+      </div>
+    );
   }
 }
 
-export default Intro
+export default Intro;
